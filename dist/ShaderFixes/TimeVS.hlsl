3778405f7e71ed8011d90c7355c574e81c68ddc1
Cukkoo <62637922+dogevspenguin@users.noreply.github.com>
2026-01-06 00:33:42 +0700

    Delete dist directory

--- dist/ShaderFixes/TimeVS.hlsl
+++
@@ -1,6 +0,0 @@
-// Vertex Shader - Name the function 'main'
-void main(uint id : SV_VertexID, out float4 pos : SV_Position)
-{
-    float2 uv = float2((id << 1) & 2, id & 2);
-    pos = float4(uv * float2(2, -2) + float2(-1, 1), 0, 1);
-}
